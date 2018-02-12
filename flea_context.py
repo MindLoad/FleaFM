ef53ee03c7d0a40492c96ad645daf2260a5e2307
MindLoad <fatgrout@gmail.com>
2018-02-12 21:52:10 +0200

    Merge context classes

--- flea_context.py
+++ flea_context.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
-# Created: 04.02.18
+# Created: 06.01.18
 # Changed: 04.02.18
 
+import sqlite3
 import logging
 import datetime
 
 
-def _log(func):
+class DBConnection:
+    """ Context manager: default settings """
+
+    def __init__(self, db_name):
+        self.db_name = db_name
+
+    def __enter__(self):
+        self.conn = sqlite3.connect(self.db_name)
+        return self.conn
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
+        if exc_val:
+            raise sqlite3.ProgrammingError
+
+
+def logs(func):
     """ Setup logging decorate function """
 
     def wrapper(message):
